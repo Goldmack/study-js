9435a84b1b0ee4df3c84883d5483a6127276fe9d
Goldmack <goldmack@yandex.ru>
2020-10-29 09:32:56 +0300

    пояснение (начало)

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// console.log(1);
+
+// alert ("Я JavaScript!");
+
+// let result = confirm("Ты здесь?");
+// console.log(result);
+
+// let answer = prompt("Сколько Вам лет?", "");
+// console.log(answer);
+
+// let aneswers = [];
+
+// aneswers[0] = prompt("Как ваше имя?", "");
+// aneswers[2] = prompt("Как ваша фамилия?", "");
+// aneswers[3] = prompt("Сколько вам лет?", "");
+
+// console.log(aneswers);
+// document.write(aneswers);
+
+
+
+
